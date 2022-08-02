4d969f42a4f6af765942f3fe699557fad63877fd
Mpisano10 <91083469+Mpisano10@users.noreply.github.com>
2022-08-02 17:59:58 +0200

    Add files via upload

---
+++ bruteforce.py
@@ -0,0 +1,15 @@
+import http.client
+
+host = input("Inserire Ip del sistema target :")
+port =input("Inserire porta del sistema target (default:80):")
+
+if(port == ''):
+        port = 80
+try:
+        connection = http.client.HTTPConnection(host, port)
+        connection.request('GET', 'http://192.168.50.101/phpHyAdmin/')
+        response = connection.getresponse()
+        print("I metodi abilitati sono:",response.status)
+        connection.close()
+except ConnectionRefusedError:
+        print("connessione fallita")
